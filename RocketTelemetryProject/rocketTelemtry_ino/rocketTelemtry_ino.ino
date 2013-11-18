6f76f79b8f6272bdc9eb4b571170f63f1d0cc484
Vaughan Schmidt <vaughan.schmidt@gmail.com>
2013-11-18 14:54:43 -0500

    Removed sensor min/max reporting

--- RocketTelemetryProject/rocketTelemtry_ino/rocketTelemtry_ino.ino
+++ RocketTelemetryProject/rocketTelemtry_ino/rocketTelemtry_ino.ino
@@ -30,12 +30,12 @@ void displaySensorDetails(void)
 {
   sensor_t sensor;
   accel.getSensor(&sensor);
-  Serial.println("---Acceleromter Detail--------------");
+  Serial.println("---Accelerometer Detail-------------");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
   Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  // Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  // Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
   Serial.println("------------------------------------");
   
@@ -44,8 +44,8 @@ void displaySensorDetails(void)
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
   Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  // Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  // Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
   Serial.println("------------------------------------");
   Serial.println("");
